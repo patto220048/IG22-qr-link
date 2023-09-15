83abc29adf666763f0b4f53ee25c73202e40e2e2
patto220048 <220048xn@yse-c.net>
2023-09-15 11:51:00 +0900

    first user

--- backend/src/database/model/userModel.js
+++ backend/src/database/model/userModel.js
@@ -1,16 +1,18 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
-const UserModel = new  Schema({
-    
-    userId:{
-          type:String,
-          require:true
-    }
-    
-    
-},{timestamps: true})
+const UserModel = new Schema(
+    {
+        username: {
+            type: String,
+            require: true,
+        },
+        password: {
+            type: String,
+            require: true,
+        },
+    },
+    { timestamps: true },
+);
 
-
-
-export default mongoose.model('User', UserModel )
+export default mongoose.model('User', UserModel);
--- backend/src/route/index.js
+++ backend/src/route/index.js
@@ -1,6 +1,8 @@
 import testRoute from "./test.js";
+import authRoute from "./auth.js";
 
 function route(app){
+    app.use("/auth", authRoute)
     app.use("/", testRoute)
 
 }
