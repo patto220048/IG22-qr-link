c234f605a77e44a46267c490a118adadd065b554
patto220048 <220048xn@yse-c.net>
2023-09-19 12:17:14 +0900

    .

--- backend/src/route/index.js
+++ backend/src/route/index.js
@@ -1,10 +1,10 @@
 import qrRoute from "./qr.js";
 import authRoute from "./auth.js";
-import linkRoute from "./link.js";
+
 function route(app){
     app.use("/auth", authRoute)
     app.use("/", qrRoute)
-    app.use("/link", linkRoute)
+   
 }
 
 export default route;
