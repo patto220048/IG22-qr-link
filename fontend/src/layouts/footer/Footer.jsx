2d9dfa51cd288c8aacf87dbc4b435e8043031b6c
ブイ ティエン ニャン <220057@yse.ac.jp>
2023-09-22 09:26:22 +0900

    test push git

--- fontend/src/layouts/footer/Footer.jsx
+++ fontend/src/layouts/footer/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
     return (
         <div className='footer'>
             <div className="footer-container">
-            
+                hello
             </div>
         </div>
     );
