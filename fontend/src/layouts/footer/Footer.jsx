071c4469dd45c4ca1ea7f2ab0b6b12126d406fea
DINH HUU PHAT <108036581+patto220048@users.noreply.github.com>
2023-10-03 11:30:21 +0900

    fix merge develop/footer

--- fontend/src/layouts/footer/Footer.jsx
+++ fontend/src/layouts/footer/Footer.jsx
@@ -3,50 +3,6 @@ import facbook from '../../assets/facebook1.svg';
 import github from '../../assets/github.svg';
 import twitter from '../../assets/twitter.svg';
 function Footer() {
-<<<<<<< HEAD
-	return (
-		<div className="footer">
-			<div className="footer-container">
-				<ul className="footer-container_item">
-					<ul className="footer-sub">
-						<h3>Group</h3>
-						<li>Blog</li>
-						<li>Engineering Blog</li>
-						<li>Marketplace</li>
-						<li>What's New</li>
-						<li>About</li>
-						<li>Press</li>
-						<li>Careers</li>
-						<li>Contact</li>
-					</ul>
-					<ul className="footer-sub">
-						<h3>Community</h3>
-						<li>Creator Services Program</li>
-						<li>Enterprise</li>
-						<li>Creator report</li>
-						<li>Charities</li>
-						<li>Creator Profile Directory</li>
-						<li>Explore Templates</li>
-					</ul>
-					<ul className="footer-icon">
-						<img className="footer-icon_items" src={facbook} alt="" />
-						<img className="footer-icon_items" src={github} alt="" />
-						<img className="footer-icon_items" src={twitter} alt="" />
-					</ul>
-
-					<div>
-						
-					</div>
-
-					{/* <ul className='footer-sub'>
-                    <h3>Support</h3>
-                        <li>Help Topics</li>
-                        <li>Getting Started</li>
-                        <li>Try it for pro</li>
-                        <li>Features & How-Tos</li>
-                        <li>FAQs</li>
-                        <li>Report a Violation</li>
-=======
     return (
         <div className="footer">
             <div className="footer-container">
@@ -58,7 +14,6 @@ function Footer() {
                         <li>About</li>
                         <li>Careers</li>
                         <li>Contact</li>
->>>>>>> f8e5daa07c82a42dc4aeb05e1dd7c65ce68ecc68
                     </ul>
                     <ul className="footer-sub">
                         <h3>Community</h3>
