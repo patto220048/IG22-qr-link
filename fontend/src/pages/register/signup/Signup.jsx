e285e457471da76ed05ae8eadefc0e89da1c8fca
patto220048 <220048xn@yse-c.net>
2023-10-04 14:10:35 +0900

    add valid signup

--- fontend/src/pages/register/signup/Signup.jsx
+++ fontend/src/pages/register/signup/Signup.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './Signup.scss';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
@@ -6,21 +6,59 @@ import axiosInstance from '../../../instance/axiosInstance';
 
 function Signup() {
     const [isLoading, setIsLoading] = useState(true);
+    const [err, setErr] = useState('');
+    console.log(err);
+    const navigate = useNavigate()
     const [values, setValues] = useState({
         username: '',
         email: '',
         password: '',
     });
-    console.log(axiosInstance)
-    console.log(values);
+    const validatePassword = (password) => {
+        // Define validation rules for password
+        const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+        return passwordRegex.test(password);
+    };
+    const validateEmail = (email) => {
+        const emailRegex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
+        return emailRegex.test(email);
+    };
     const handleSubmit = (e) => {
         e.preventDefault();
-        const data = axiosInstance.post(`/auth/signup`, {
-            username: values.username,
-            email: values.email,
-            password: values.password,
-        })
-        console.log(data);
+        const signup = async () => {
+            // valid email and password
+            const validPassword = validatePassword(password);
+            const validEmail = validateEmail(email);
+            try {
+                if (validPassword && validEmail === true) {
+                    const res = await axiosInstance.post(`/auth/signup`, {
+                        username: values.username,
+                        email: values.email,
+                        password: values.password,
+                    });
+                    // isloading -> false
+                    setIsLoading(false);
+                    console.log(res.data);
+                    navigate('/register/login')
+                  
+                }
+                else if(validEmail===false){
+                    setErr("Email incorrect!")
+                }
+                else if(validPassword===false){
+                    setErr("Password min 8 chars!")
+                }
+               
+                
+            } catch (error) 
+            {
+                console.log(error.message)
+            }
+            if(isLoading){
+                return <>Loading....</>
+            }
+        };
+        signup();
     };
     // onchange input
     const onChange = (e) => {
@@ -44,7 +82,7 @@ function Signup() {
                         onChange={onChange}
                     />
 
-                    {/* <p className="input-err">12</p> */}
+                   {err ? <p className="input-err">{err}</p>: <></>}
                     <input
                         className="signup-input"
                         type="text"
