2497e3c9cc184c79c08a44d0d992571c6e37fe09
patto220048 <220048xn@yse-c.net>
2023-11-29 10:45:44 +0900

    fix svg icon

--- fontend/src/svg/icon.jsx
+++ fontend/src/svg/icon.jsx
@@ -202,9 +202,9 @@ export const upLoadVideo = (w, h) => (
         <path
             d="M4 15.2044V18.8925C4 19.4514 4.21071 19.9875 4.58579 20.3827C4.96086 20.778 5.46957 21 6 21H18C18.5304 21 19.0391 20.778 19.4142 20.3827C19.7893 19.9875 20 19.4514 20 18.8925V15.2044M12.0007 14.9425L12.0007 3M12.0007 3L7.42931 7.56318M12.0007 3L16.5722 7.56318"
             stroke="black"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
         />
     </svg>
 );
