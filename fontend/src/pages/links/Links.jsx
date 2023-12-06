b646adcb385cfb178ce9fd38511025bfc37cafb9
patto220048 <220048xn@yse-c.net>
2023-12-06 11:38:06 +0900

    update add,delete link

--- fontend/src/pages/links/Links.jsx
+++ fontend/src/pages/links/Links.jsx
@@ -5,24 +5,33 @@ import { plusIcon } from '../../svg/icon';
 import './Links.scss';
 import AddLink from '../../components/AddLink/AddLink';
 import { useSelector } from 'react-redux';
+import Loading from '../../components/dialog/loading/Loading';
 
 function Links() {
-    const {loading}= useSelector((state)=> state.url.currentUrl)
-    const [isAddLink, setIsAddLink] = useState(false)
+    const loading = useSelector((state) => state.url.loading);
+    const [isAddLink, setIsAddLink] = useState(false);
     const handleAddLink = () => {
         setIsAddLink(true);
-    }
+    };
     return (
         <div className="Links">
             <div className="Links-left">
                 <div className="Links-contents">
-                    {!isAddLink && <button className="Links-left-btn" onClick={handleAddLink}>{plusIcon(30, 30)} Add Link</button>}
-                    {isAddLink && <AddLink  isAddLink= {isAddLink} setIsAddLink={setIsAddLink}/>}
+                    {!isAddLink && (
+                        <button className="Links-left-btn" onClick={handleAddLink}>
+                            {loading ? (
+                                <Loading urlLoading = {true} isLoading={loading} />
+                            ) : (
+                                <>{plusIcon(30, 30)} Add link</>
+                            )}
+                        </button>
+                    )}
+                    {isAddLink && <AddLink isAddLink={isAddLink} setIsAddLink={setIsAddLink} />}
                     <section className="Links-left-wapper">
                         <LinksItems />
                     </section>
+                    {/* <Loading isLoading = {loading} /> */}
                 </div>
-         
             </div>
             {/* preview */}
             <div className="Links-right">
